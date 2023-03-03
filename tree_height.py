f23a8f7e695cc8ecf55aa11660a698baef6b09b3
RaitisBertulis <125643206+RaitisBertulis@users.noreply.github.com>
2023-03-03 15:32:12 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -19,31 +19,29 @@ def compute_height(n, parents):
             skaits = 1 + skaits
             pasr = parents[pasr]
         max_height[i] = skaits
-    return max(max_height)
-
-
+        max_height = max(max_height)
+    return max_height
 
 def main():
     # implement input form keyboard and from files
     cmd = input()
-    if "I" in cmd:
-        n=int(input())
-        #parents = list(map(int, input().split()))
-        print(compute_height(n, parents))
     # let user input file name to use, don't allow file names with letter a
-    elif "F" in cmd:
     # account for github input inprecision
-        nos=input()
+    if "F" in cmd:
+        nos = input()
         if "a" not in nos:
-            with open(f"test/{nos}") as f:
+            with open(f"test/{nos}") as file
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
-                n = int(f.readline().strip())
-                parents=list(map(int, f.readline().strip().split()))
     # call the function and output it's result
-    print(compute_height(n, parents))
-    
-
+                n = int(file.readline().strip())
+                parents=list(map(int, file.readline().strip().split()))
+                print(compute_height(n, parents))
+    elif "I" in cmd:
+        n=int(input())
+        parents = list(map(int, input().split()))
+        print(compute_height(n, parents))
+        
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
